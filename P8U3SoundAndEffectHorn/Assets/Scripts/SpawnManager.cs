de350a599d43ec4a971f0ee8ad4f694ce48d2cd3
Rafaelakamexican <739415@student.dusd.net>
2025-05-06 15:09:43 -0700

    Assets/Scripts/SpawnManager.cs(22,13): error CS0229: Ambiguity between 'UnityEngine.Object.Instantiate(UnityEngine.Object, UnityEngine.Vector3, UnityEngine.Quaternion)' and 'UnityEngine.Object.Instantiate<T>(T, UnityEngine.Vector3, UnityEngine.Quaternion)' -- consider casting one of the arguments to help the compiler resolve the overload

--- P8U3SoundAndEffectHorn/Assets/Scripts/SpawnManager.cs
+++ P8U3SoundAndEffectHorn/Assets/Scripts/SpawnManager.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawnmanager : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
-    private float repeatRate  = 2;
+    private float repeatRate = 2;
+
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObsstacle", startDelay, repeatRate);
+
     }
 
     // Update is called once per frame
